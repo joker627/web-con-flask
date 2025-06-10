ea2feb7e9f92f3aac022189e7734516b7d1a4903
joker627 <jorgemanuelsantanablanco2@gmail.com>
2025-06-10 14:33:47 -0500

    web-flask-formulario

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -7,6 +7,32 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/lenguajes")
+def lenguajes():
+    return render_template("lenguajes.html")
+
+@app.route("/contacto", methods=['GET', 'POST']) 
+def contacto():
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        email = request.form['email']
+        asunto = request.form.get('asunto', 'Sin Asunto')
+        mensaje = request.form['mensaje']
+
+        print(f"Mensaje recibido de: {nombre}")
+        print(f"Email: {email}")
+        print(f"Asunto: {asunto}")
+        print(f"Mensaje:\n{mensaje}\n---")
+
+        return "¡Mensaje recibido con éxito! Gracias por contactarnos."
+    else:
+        return render_template("contacto.html")
+
+@app.route("/politica")
+def politica():
+    return render_template("politica.html")
+
+
 # Manejador de errores 404
 @app.errorhandler(404)
 def page_not_found(error):
@@ -14,3 +40,6 @@ def page_not_found(error):
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
